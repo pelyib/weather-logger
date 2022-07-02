3b9e3d5881809b54762097b0f77dc9b7ef02de86
Botond Pelyi <botond@pelyi.hu>
2022-07-02 22:57:52 +0200

    refactor: print only the prefix in builder script

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -2,12 +2,13 @@
 
 FILENAME_SUBFIX=_${GOOS}_${GOARCH}
 
+echo ${FILENAME_SUBFIX}
+echo 
+echo =======   HTTP   =======
 go build -o /app/bin/http${FILENAME_SUBFIX} /app/cmd/http
 echo 
-echo ======= LOGGER =======
-printenv
+echo =======  LOGGER  =======
 go build -o /app/bin/logger${FILENAME_SUBFIX} /app/cmd/logger
 echo
 echo ====== COMMANDER =======
-printenv
 go build -o /app/bin/commander${FILENAME_SUBFIX} /app/cmd/commander
