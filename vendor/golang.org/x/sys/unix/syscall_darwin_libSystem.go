a86c8b94f3250fbc4e9c997824e2aab667f21156
Botond Pelyi <botond.pelyi@gmail.com>
2024-12-22 22:19:23 +0100

    feat: update dependencies

--- vendor/golang.org/x/sys/unix/syscall_darwin_libSystem.go
+++ vendor/golang.org/x/sys/unix/syscall_darwin_libSystem.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin && go1.12
 // +build darwin,go1.12
 
 package unix
@@ -27,6 +28,7 @@ func syscall_syscallPtr(fn, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno)
 
 // Find the entry point for f. See comments in runtime/proc.go for the
 // function of the same name.
+//
 //go:nosplit
 func funcPC(f func()) uintptr {
 	return **(**uintptr)(unsafe.Pointer(&f))
